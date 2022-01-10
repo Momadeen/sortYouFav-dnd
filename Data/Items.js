1f03d842f2aa2a33a5f53fd96dcd30aa507c332a
Momadeen <madienxz@gmail.com>
2022-01-10 11:57:03 +0200

    create dnd app for sort your fav food

---
+++ Data/Items.js
@@ -0,0 +1,52 @@
+import { v4 as uuid } from "uuid";
+
+export const items = [
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Fried Pepper",
+    price: 20,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Lime Mammoth",
+    price: 62,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Thermal-Cooked",
+    price: 25,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/2732666/pexels-photo-2732666.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Shallow-Fried",
+    price: 28,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/2136862/pexels-photo-2136862.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Gentle-Fried",
+    price: 35,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/718739/pexels-photo-718739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Steamed Sweet",
+    price: 46,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/2777627/pexels-photo-2777627.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Spicy Bread",
+    price: 24,
+  },
+  {
+    id: uuid(),
+    img: "https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Slow-Cooked",
+    price: 50,
+  },
+];
